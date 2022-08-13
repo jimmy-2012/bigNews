7dfba17ac2ee0f832a7670d6cc2fc72a79dde7d5
lemontest-qcb(Sharonli9081) <342379177@QQ.COM>
2022-08-13 20:15:00 +0800

    完成了主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,18 @@
 $.ajaxPrefilter(function(opt){
 
-    opt.url = 'http://www.liulongbin.top:3007' + opt.url
-    
+    opt.url = 'http://www.liulongbin.top:3007' + opt.url  
+
+    if(opt.url.indexOf('/my/') !== -1){
+        opt.headers = {
+            Authorization:localStorage.getItem('token') || ''
+        } 
+    }
+
+    opt.complete = function(res) {
+            
+        if(res.responseJSON.status == 1){
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+$(function(){
+    getUserinfo()
+
+    //退出登录
+    $('#loginOut').on('click',function(){
+        layui.layer.confirm('是否退出登录', {icon: 3, title:'提示'}, function(index){
+            //do something
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index)
+        })
+    })
+})
+
+function getUserinfo(){
+    $.ajax({
+        method:'GET',
+        url:'/my/userinfo', 
+        success:function(res){
+            if(res.status !== 0){
+                return layui.layer.msg(res.message)
+            }
+            renderUser(res.data)
+        }
+    })
+}
+
+//渲染用户信息
+function renderUser(user){
+    let name = user.username || user.nickname
+
+    $('.welcome').html('欢迎 &nbsp;&nbsp;'+ name)
+
+    if(user.user_pic !==null){
+        $('.layui-nav-img').attr('src',user.user_pic).show() 
+        $('.welcome').hide() 
+
+    }else{
+        $('.layui-nav-img').hide()
+        $('.welcome').show()  
+        let first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+
+}
+
